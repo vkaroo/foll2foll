0b06db6699971ae21ce7e83e2a5206dcd39c52ec
vkaroo <vkar29@gmail.com>
2025-04-06 22:01:19 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,12 @@ export default function Home() {
         <meta name="fc:frame" content="vNext" />
         <meta name="fc:frame:creator" content="302592" />
         <meta name="fc:frame:image" content="https://raw.githubusercontent.com/vkaroo/foll2foll/main/handshake.png" />
-        <meta name="fc:frame:button:1" content="Share" />
-        <meta name="fc:frame:post_url" content="https://foll2foll.vercel.app/api/share" />
+
+        <meta name="fc:frame:button:1" content="Follow Me" />
+        <meta name="fc:frame:button:2" content="Share" />
+        
+        <meta name="fc:frame:post_url" content="https://warpcast.com/~/profile/302592" />
+        <meta name="fc:frame:button:2:action" content="share" />
       </Head>
 
       <main>
