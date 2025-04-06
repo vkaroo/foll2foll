1c9d0c88ec904b1cfd056c19569294d599b877d8
vkaroo <vkar29@gmail.com>
2025-04-06 22:15:15 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,15 @@ export default function Home() {
         <meta property="og:image" content="https://raw.githubusercontent.com/vkaroo/foll2foll/main/handshake.png" />
         
         <meta name="fc:frame" content="vNext" />
+        <meta name="fc:frame:version" content="vNext" />
         <meta name="fc:frame:creator" content="302592" />
         <meta name="fc:frame:image" content="https://raw.githubusercontent.com/vkaroo/foll2foll/main/handshake.png" />
 
         <meta name="fc:frame:button:1" content="Follow Me" />
+        <meta name="fc:frame:button:1:action" content="link" />
+        <meta name="fc:frame:button:1:target" content="https://warpcast.com/~/profile/302592" />
+
         <meta name="fc:frame:button:2" content="Share" />
-        
-        <meta name="fc:frame:post_url" content="https://warpcast.com/~/profile/302592" />
         <meta name="fc:frame:button:2:action" content="share" />
       </Head>
 
